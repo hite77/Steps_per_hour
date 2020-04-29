bf239b8125b7a18c1288c976a8c95305af1c0c03
Jason Hite <hite77@gmail.com>
2020-04-29 17:02:22 -0400

    Update to have step goal updated, hide as each hour goes, and update icons for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:flutter/material.dart';
 import 'package:clock/clock.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,63 +14,85 @@ class MyApp extends StatelessWidget {
 }
 
 class StepsPerHourState extends State<StepsPerHour> {
-  var hour = clock.now().hour,
-
-  var entries = <String>[];
-    if (hour >= 7) { entries.add('7  -->   715 steps')};
-    '8  -->  1430 steps',
-    '9  -->  2145 steps',
-    '10 -->  2860 steps',
-    '11 -->  3575 steps',
-    '12 -->  4290 steps',
-    ' 1 -->  5005 steps',
-    ' 2 -->  5720 steps',
-    ' 3 -->  6435 steps',
-    ' 4 -->  7150 steps',
-    ' 5 -->  7865 steps',
-    ' 6 -->  8580 steps',
-    ' 7 -->  9295 steps',
-    ' 8 --> 10000 steps'
-  ];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  var goalsteps = 12000;
 
   Widget _buildStepsPerHour() {
+    var hour = clock.now().hour;
+    var entries = <String>[];
+    var increase = (goalsteps / 14).floor();
+    if (hour <= 7) entries.add("7  -->   $increase steps");
+    if (hour <= 8) entries.add("8  -->   ${increase * 2} steps");
+    if (hour <= 9) entries.add("9  -->   ${increase * 3} steps");
+    if (hour <= 10) entries.add("10 -->  ${increase * 4} steps");
+    if (hour <= 11) entries.add("11 -->  ${increase * 5} steps");
+    if (hour <= 12) entries.add("12 -->  ${increase * 6} steps");
+    if (hour <= 13) entries.add("1  -->  ${increase * 7} steps");
+    if (hour <= 14) entries.add("2  -->  ${increase * 8} steps");
+    if (hour <= 15) entries.add("3  -->  ${increase * 9} steps");
+    if (hour <= 16) entries.add("4  -->  ${increase * 10} steps");
+    if (hour <= 17) entries.add("5  -->  ${increase * 11} steps");
+    if (hour <= 18) entries.add("6  -->  ${increase * 12} steps");
+    if (hour <= 19) entries.add("7  -->  ${increase * 13} steps");
+    if (hour <= 20) entries.add("8  --> $goalsteps steps");
     return ListView.builder(
         padding: const EdgeInsets.all(2.0),
         itemBuilder: (context, i) {
-          if (i < entries.length) {
-            return _buildRow(entries[i]);
+          if (i == 0) {
+            return Row(
+              children: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.arrow_upward),
+                    onPressed: () {
+                      setState(() {
+                        goalsteps += 1000;
+                      });
+                    }),
+                Expanded(
+                  child: Center(
+                    child: Text("$goalsteps"),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.arrow_downward),
+                    onPressed: () {
+                      setState(() {
+                        goalsteps -= 1000;
+                      });
+                    }),
+              ],
+            );
           }
-          else
+          if (i <= entries.length) {
+            return _buildRow(entries[i - 1]);
+          } else
             return ListTile();
         });
   }
 
   Widget _buildRow(String text) {
     return ListTile(
-        title: Text(
-          text,
-          style: _biggerFont,
-        )
+      title: Text(
+        text,
+        style: _biggerFont,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Steps Per Hour'),
+        appBar: AppBar(
+          title: Text('Steps Per Hour'),
           actions: <Widget>[
-      // action button
-      IconButton(
-      icon: Icon(Icons.adjust),
-      onPressed: () {
-//        ;
-      },
-    ),
-      ]),
-          body: _buildStepsPerHour(),
-    );
+            IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () {
+                  setState(() {});
+                }),
+          ],
+        ),
+        body: _buildStepsPerHour());
   }
 }
 
@@ -82,4 +100,3 @@ class StepsPerHour extends StatefulWidget {
   @override
   StepsPerHourState createState() => StepsPerHourState();
 }
-
